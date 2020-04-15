ca6150dacdbffa430ba81e9258b9f94166ae4740
TrivilinDev <trivilin.dev@hotmail.com>
2020-04-15 10:29:55 -0300

    Fixing merge problem

--- src/players/VideoDefault/InitialPlay.js
+++ src/players/VideoDefault/InitialPlay.js
@@ -5,13 +5,9 @@ import {
 } from 'react-native'
 import { boxStyle } from '../../commons'
 
-<<<<<<< HEAD
 import Icon from '../../Icon'
 
 export default PlayButton = ({togglePause}) => (
-=======
-export default InitialPlay = ({togglePause}) => (
->>>>>>> 8c08668d698ffd59761c0d6274070b7ca64c436e
     <View style={boxStyle}>
         <TouchableOpacity 
             onPress={togglePause}
